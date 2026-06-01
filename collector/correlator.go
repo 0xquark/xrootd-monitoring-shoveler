bd29ad2f3b1933d460cb953a43a1bfa59a269ba1
Karanjot Singh <karanjot.singh@cern.ch>
2026-06-01 17:23:05 +0200

    feat: add metrics for f-stream record types

--- collector/correlator.go
+++ collector/correlator.go
@@ -438,6 +438,7 @@ func (c *Correlator) ProcessPacket(packet *parser.Packet) ([]*CollectorRecord, e
 	for _, rec := range packet.FileRecords {
 		switch r := rec.(type) {
 		case parser.FileOpenRecord:
+			fstreamFileOpenRecordsTotal.WithLabelValues(serverIP).Inc()
 			result, err := c.handleFileOpen(r, packet, serverID, windowBeg)
 			if err != nil {
 				return records, err
@@ -446,6 +447,7 @@ func (c *Correlator) ProcessPacket(packet *parser.Packet) ([]*CollectorRecord, e
 				records = append(records, result)
 			}
 		case parser.FileCloseRecord:
+			fstreamFileCloseRecordsTotal.WithLabelValues(serverIP).Inc()
 			result, err := c.handleFileClose(r, packet, serverID, windowBeg, windowEnd)
 			if err != nil {
 				return records, err
@@ -454,6 +456,7 @@ func (c *Correlator) ProcessPacket(packet *parser.Packet) ([]*CollectorRecord, e
 				records = append(records, result)
 			}
 		case parser.FileTimeRecord:
+			fstreamFileTimeRecordsTotal.WithLabelValues(serverIP).Inc()
 			// The window was already extracted above; the FileTOD record itself
 			// does not correlate to a file operation.
 		case parser.FileDisconnectRecord:
